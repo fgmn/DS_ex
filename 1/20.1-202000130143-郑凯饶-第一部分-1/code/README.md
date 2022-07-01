12e8a44ef292d8b50a5f15e6d10c436254453141
fgmn <1076802156@qq.com>
2022-07-01 10:45:56 +0800

    DS_ex

---
+++ 1/20.1-202000130143-郑凯饶-第一部分-1/code/README.md
@@ -0,0 +1,6 @@
+### 跳表实验项目说明
+
+1. 使用 $ CMake $ 编译项目；
+2. 跳表节点实现头文件 $ skipNode.h $ 及跳表定义头文件 $ skipList.cpp $ 位于 $ code/CMakeTest/include $ 目录下；
+3. 跳表实现 $ skipList.cpp $ 及测试运行程序 $ main.cpp $ 位于 $ code/CMakeTest/src $ 目录下；
+4. 用于和标准答案比对的法官程序 $ localJudge $ 位于 $ code $ 目录下。
---
+++ 2/20.1-202000130143-郑凯饶-第一部分-2/code/LoserTree/build/CMakeFiles/LoserTree.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../bin/LoserTree.exe"
+  "../bin/LoserTree.exe.manifest"
+  "../bin/LoserTree.pdb"
+  "../bin/libLoserTree.dll.a"
+  "CMakeFiles/LoserTree.dir/src/LoserTree.obj"
+  "CMakeFiles/LoserTree.dir/src/LoserTree.obj.d"
+  "CMakeFiles/LoserTree.dir/src/main.obj"
+  "CMakeFiles/LoserTree.dir/src/main.obj.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LoserTree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
